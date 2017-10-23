37d98ca04d2a92cc6092b523266a38f4c34b074d
Luis Méndez <luis.rains@hotmail.com>
2017-10-23 08:29:02 -0300

    video35

---
+++ #Comandos Angular.js
@@ -0,0 +1,16 @@
+#Comandos Angular
+
+Pre-requisitos
+1- Instalar NodeJs 6.x 
+2- Instalar Npm 3.x
+3- Instalar Angular/cli 
+
+
+1- npm install -g @angular/cli
+2- ng new CursoAngular
+3- cd CursoAngular -> npm start
+
+#Tutorial
+https://angular.io
+
+ver video 35

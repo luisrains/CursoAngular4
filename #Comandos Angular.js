a077e39562587026c9e537f2e938c97078472e17
Luis Méndez <luis.rains@hotmail.com>
2017-11-01 08:17:15 -0300

    comandos angular

--- #Comandos Angular.js
+++ #Comandos Angular.js
@@ -14,6 +14,19 @@ Pre-requisitos
 https://angular.io
 
 ver video 35
+ver video 41 para instalar dependencias
+
+## para iniciar el mongo
+service mongod start
+y
+/usr/local/bin/robomongo/
+./robo3t
+
 
 //para usar el midleware
 hay que poner en cada uno de las rutas, como segundo parametro
+
+//al crear un controlador
+1 creamos el controlador en el folder controllers
+2 vamos routes y creamos el route de ese controlador
+3 en app.js hay que crearle un segmente de /api al controller
